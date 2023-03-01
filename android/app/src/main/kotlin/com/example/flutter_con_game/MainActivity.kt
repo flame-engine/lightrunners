5761df2e021f501a5718612f64cc459d478b865b
Luan Nico <luanpotter27@gmail.com>
2023-03-01 01:49:22 -0500

    ignite create

---
+++ android/app/src/main/kotlin/com/example/flutter_con_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_con_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget(game: MyGame()));
+}
+
+class MyGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    // TODO add your load logic
+    return super.onLoad();
+  }
+}
