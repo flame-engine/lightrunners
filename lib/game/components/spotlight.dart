86b7df6eb0c0747ee8c4a2de9b0dfff981f01c3d
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2023-05-28 23:15:08 +0200

    fix: Clean up spotlight (#41)
    * Clean up spotlight
    
    * No abbrevations

--- lib/game/components/spotlight.dart
+++ lib/game/components/spotlight.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/extensions.dart';
 import 'package:flame/palette.dart';
 import 'package:lightrunners/game/components/ship.dart';
@@ -13,33 +13,34 @@ const _spotlightRadius = 35.0; //pixels
 const _spotlightSpeed = 50.0; // pixels per second
 const _targetChangeProbability = 0.05; // per [_targetChangePeriod]
 const _targetChangePeriod = 0.5; // seconds
-final _r = Random();
+final _random = Random();
 
-class Spotlight extends PositionComponent with HasGameRef<LightRunnersGame> {
-  Paint paint = Paint();
+class Spotlight extends PositionComponent
+    with HasGameReference<LightRunnersGame>, HasPaint {
+  Spotlight()
+      : super(
+          size: Vector2.all(_spotlightRadius * 2),
+          anchor: Anchor.center,
+        );
 
   double scoringCounter = 0.0;
   Ship? currentShipRef;
 
+  final Vector2 target = Vector2.zero();
+  final _renderPosition = Vector2.all(_spotlightRadius).toOffset();
   double targetChangeCounter = 0.0;
-  Vector2 newTarget = Vector2.zero();
 
   @override
   Future<void> onLoad() async {
-    anchor = Anchor.center;
-    size = Vector2.all(_spotlightRadius * 2);
-    position = Vector2.zero();
-    newTarget = newRandomTarget();
+    updateRandomTarget();
     _updateColor();
-    return super.onLoad();
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-
     _maybeUpdateTarget(dt);
-    moveTowards(position, newTarget, _spotlightSpeed * dt);
+    moveTowards(position, target, _spotlightSpeed * dt);
 
     _updateCurrentShip();
     _computeScoring(dt);
@@ -49,8 +50,8 @@ class Spotlight extends PositionComponent with HasGameRef<LightRunnersGame> {
     targetChangeCounter += dt;
     while (targetChangeCounter > _targetChangePeriod) {
       targetChangeCounter -= _targetChangePeriod;
-      if (_r.nextDouble() <= _targetChangeProbability) {
-        newTarget = newRandomTarget();
+      if (_random.nextDouble() <= _targetChangeProbability) {
+        updateRandomTarget();
       }
     }
   }
@@ -61,7 +62,6 @@ class Spotlight extends PositionComponent with HasGameRef<LightRunnersGame> {
       scoringCounter -= 1.0;
       currentShipRef?.score++;
     }
-    // scoring
   }
 
   void _updateColor() {
@@ -79,7 +79,7 @@ class Spotlight extends PositionComponent with HasGameRef<LightRunnersGame> {
   }
 
   Ship? _decideCurrentShip() {
-    final inside = gameRef.ships.values.where((ship) {
+    final inside = game.ships.values.where((ship) {
       final distance = ship.absolutePosition.distanceTo(absolutePosition);
       return distance < _spotlightRadius;
     });
@@ -90,18 +90,18 @@ class Spotlight extends PositionComponent with HasGameRef<LightRunnersGame> {
     }
   }
 
-  Vector2 newRandomTarget() {
-    final rect = gameRef.playArea.deflate(_spotlightRadius);
-    final x = -rect.width / 2 + _r.nextDouble() * rect.width;
-    final y = -rect.height / 2 + _r.nextDouble() * rect.height;
-    return Vector2(x, y);
+  void updateRandomTarget() {
+    final rect = game.playArea.deflate(_spotlightRadius);
+    final x = -rect.width / 2 + _random.nextDouble() * rect.width;
+    final y = -rect.height / 2 + _random.nextDouble() * rect.height;
+    target.setValues(x, y);
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
     canvas.drawCircle(
-      Vector2.all(_spotlightRadius).toOffset(),
+      _renderPosition,
       _spotlightRadius,
       paint,
     );
