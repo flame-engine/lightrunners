cce67ba73e51f6eae94850d470d68f044c3e7b8b
Erick <erickzanardoo@gmail.com>
2023-05-28 18:24:07 -0300

    feat: Adding color palette (#42)
    feat: adding color palette

---
+++ lib/ui/palette.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class GamePalette {
+  static const red = Color(0xFFE03232);
+  static const orange = Color(0xFFE07B32);
+  static const yellow = Color(0xFFE0D932);
+  static const green = Color(0xFF89E032);
+  static const lightBlue = Color(0xFF32D6E0);
+  static const blue = Color(0xFF3263E0);
+  static const pink = Color(0xFFE032CF);
+  static const silver = Color(0xFFCFCFCF);
+
+  static const values = [
+    red,
+    orange,
+    yellow,
+    green,
+    lightBlue,
+    blue,
+    pink,
+    silver,
+  ];
+}
