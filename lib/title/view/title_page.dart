880191ff56bcf0b6bde690641df46e0754197790
Luan Nico <luanpotter27@gmail.com>
2023-05-28 16:51:44 -0400

    fix: Fix anchor & lack of entrypoint (#37)

--- lib/title/view/title_page.dart
+++ lib/title/view/title_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lightrunners/game/view/game_page.dart';
 import 'package:lightrunners/widgets/screen_scaffold.dart';
 
 class TitlePage extends StatelessWidget {
@@ -6,9 +7,17 @@ class TitlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ScreenScaffold(
-      child: Center(
-        child: Text('Title Page'),
+    return ScreenScaffold(
+      child: Column(
+        children: [
+          const Text('Title Page'),
+          ElevatedButton(
+            child: const Text('Go to Game Page'),
+            onPressed: () {
+              Navigator.of(context).push(GamePage.route());
+            },
+          ),
+        ],
       ),
     );
   }
