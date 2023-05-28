9892d999ee37256fbc210209ef46c8dacad69f6d
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-05-28 20:36:46 +0100

    feat: Add screen scaffold

---
+++ lib/widgets/screen_scaffold.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ScreenScaffold extends StatelessWidget {
+  const ScreenScaffold({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF000000),
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(13),
+          ),
+          color: Color(0xFF89E032),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(13),
+            ),
+            child: ColoredBox(
+              color: const Color(0xFF000000),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.asset(
+                    'assets/images/bg.png',
+                    fit: BoxFit.cover,
+                  ),
+                  child,
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
