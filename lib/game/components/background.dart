cd076707029427596fbfef58a267c179cde8a26b
Luan Nico <luanpotter27@gmail.com>
2023-06-11 13:01:36 -0400

    next iteration

--- lib/game/components/background.dart
+++ lib/game/components/background.dart
@@ -7,7 +7,7 @@ import 'package:flame/extensions.dart';
 import 'package:flame/geometry.dart';
 import 'package:lightrunners/game/lightrunners_game.dart';
 
-const _minTriangleAngle = pi / 16;
+const _minTriangleAngle = pi / 16 / 2;
 const _epsilon = 1e-3;
 
 class Background extends SpriteComponent with HasGameRef<LightRunnersGame> {
@@ -17,21 +17,28 @@ class Background extends SpriteComponent with HasGameRef<LightRunnersGame> {
   @override
   Future<void> onLoad() async {
     size = gameRef.playArea.size.toVector2();
-    sprite = await _generateSprite(size, 100);
+    sprite = await _generateSprite(size, 200);
   }
 
   Future<Sprite> _generateSprite(Vector2 size, int n) async {
-    final (points, triangles) = _generateTriangularMesh(size, n);
+    final (points, triangles, t2) = _generateTriangularMesh(size, n);
 
     final recorder = PictureRecorder();
     final canvas = Canvas(recorder);
 
     final paint = Paint()
       ..color = const Color(0xFF00FFFF)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4;
+
+    final paint2 = Paint()
+      ..color = const Color(0xFFFFFF00)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 4;
 
+    // canvas.translateVector(-size);
     triangles.forEach((t) => canvas.drawPath(t.toPath(), paint));
+    t2.forEach((t) => canvas.drawPath(t.toPath(), paint2));
     points.forEach((p) => canvas.renderPoint(p, size: 5));
 
     final picture = recorder.endRecording();
@@ -43,30 +50,47 @@ class Background extends SpriteComponent with HasGameRef<LightRunnersGame> {
     return Sprite(image);
   }
 
-  (Iterable<Vector2>, List<_Triangle>) _generateTriangularMesh(
-      Vector2 size, int n) {
+  (Iterable<Vector2>, List<_Triangle>, List<_Triangle>) _generateTriangularMesh(
+    Vector2 size,
+    int n,
+  ) {
     final allPoints = List.generate(n, (_) => Vector2.random()..multiply(size));
     final points = allPoints.whereIndexed((i, p) {
       final otherPoints = [...allPoints]..removeAt(i);
       return !_isAnyCollinear(p, otherPoints);
     }).sortedBy<num>((e) => e.x);
-    print(points.length);
-    final triangles = <_Triangle>[
-      _Triangle(vertices: points.take(3).toList()),
-    ];
-    for (final p in points.skip(3)) {
-      final newTriangles = triangles
+    final triangles = _buildTriangleBorder(size);
+    final t2s = <_Triangle>[];
+    for (final p in points) {
+      final potentialTriangles = triangles
+          .sortedBy<num>((e) => e.centroid.distanceTo(p))
           .map((e) => e.vertices.sortedBy<num>(p.distanceTo).take(2))
           .where((vs) => vs.every((v) => _canSeePoint(p, v, triangles)))
           .map((vs) => _Triangle(vertices: [p, ...vs]))
-          .where((t) => t.angles.every((a) => a >= _minTriangleAngle))
-          .take(1)
           .toList();
-      print('adding ${newTriangles.length}');
+      final newTriangles = potentialTriangles
+          .where((t) => t.angles.every((a) => a >= _minTriangleAngle))
+          // .where((t) => triangles.none((existing) => !_overlaps(t, existing)))
+          .take(3)
+          .toList();
+      t2s.addAll(potentialTriangles);
       triangles.addAll(newTriangles);
     }
-    print(triangles.length);
-    return (points, triangles);
+    return (points, triangles, []); // t2s);
+  }
+
+  List<_Triangle> _buildTriangleBorder(Vector2 size) {
+    const n = 10;
+    final p1s = List.generate(n, (idx) => idx * size.y / n)
+        .map((e) => Vector2(0, e))
+        .toList();
+    final p2s = List.generate(n - 1, (idx) => (idx + 0.5) * size.y / n)
+        .map((e) => Vector2(20, e))
+        .toList();
+    return [
+      for (var i = 0; i < n - 1; i++)
+        _Triangle(vertices: [p1s[i], p2s[i], p1s[i + 1]])
+    ];
   }
 
   bool _isAnyCollinear(Vector2 p, Iterable<Vector2> points) {
@@ -92,6 +116,10 @@ class Background extends SpriteComponent with HasGameRef<LightRunnersGame> {
   bool _equal(Vector2 v1, Vector2 v2) {
     return v1 == v2 || (v1 - v2).length < _epsilon;
   }
+
+  bool _overlaps(_Triangle t1, _Triangle t2) {
+    return t1.isTriangleInside(t2) || t2.isTriangleInside(t1);
+  }
 }
 
 class _Triangle {
@@ -100,6 +128,14 @@ class _Triangle {
   _Triangle({
     required this.vertices,
   });
+
+  Vector2 get v1 => vertices[0];
+  Vector2 get v2 => vertices[1];
+  Vector2 get v3 => vertices[2];
+
+  Vector2 get centroid {
+    return vertices.reduce((v1, v2) => v1 + v2)..scale(1 / vertices.length);
+  }
 
   Vector2 _modGet(int idx) => vertices[idx % vertices.length];
 
@@ -119,5 +155,24 @@ class _Triangle {
 
   List<Vector2> intersections(LineSegment line) {
     return edges.expand((e) => e.intersections(line)).toList();
+  }
+
+  double _sign(Vector2 p1, Vector2 p2, Vector2 p3) {
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+  }
+
+  bool isTriangleInside(_Triangle other) {
+    return other.vertices.any(containsPoint);
+  }
+
+  bool containsPoint(Vector2 p) {
+    final d1 = _sign(p, v1, v2);
+    final d2 = _sign(p, v2, v3);
+    final d3 = _sign(p, v3, v1);
+
+    final anyNegative = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    final anyPositive = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+    return !(anyNegative && anyPositive);
   }
 }
