7cce6174e039375f5a95d076024ae22a59607475
Lukas Klingsbo <me@lukas.fyi>
2023-06-30 12:02:38 +0200

    Full screen linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -47,7 +47,8 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "lightrunners");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  //gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_fullscreen(GTK_WINDOW(window));
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
